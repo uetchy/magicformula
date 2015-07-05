34fea632ca93daa2919c56bf7f600cbfe467a2c8
Yasuaki Uechi <uetchy@randompaper.co>
2015-07-06 02:26:44 +0900

    Fetch SHA from previous commit

--- command_push.go
+++ command_push.go
@@ -58,7 +58,7 @@ func toCamelCase(str string) string {
   return strings.Title(res)
 }
 
-func hashSum(path string) string {
+func hashFileSum(path string) string {
   hasher := sha1.New()
   f, err := os.Open(path)
   if err != nil {
@@ -73,48 +73,78 @@ func hashSum(path string) string {
 }
 
 func doPush(c *cli.Context) {
-  githubToken := c.String("github-token")
+  githubToken := c.String("token")
   packageName := c.String("name")
   packageOwner := c.String("owner")
   packageVersion := c.String("version")
   binaryPath64 := c.String("package-path-64")
   binaryPath32 := c.String("package-path-32")
-
+  // TODO: load from flags
+  commitMessage := "test"
+  commitAuthor := "Yasuaki Uechi"
+  commitAuthorEmail := "uetchy@randompaper.co"
   releaseTag := c.String("tag")
   if releaseTag == "" {
     releaseTag = "v" + packageVersion
   }
-  packageUrl64 := "https://github.com/" + packageOwner + "/" + packageName + "/releases/download/" + releaseTag + "/" + binaryPath64
-  packageHash64 := hashSum(binaryPath64)
-  packageUrl32 := "https://github.com/" + packageOwner + "/" + packageName + "/releases/download/" + releaseTag + "/" + binaryPath32
-  packageHash32 := hashSum(binaryPath32)
 
+  // Generate formula
+  githubRepoUrl := "https://github.com/" + packageOwner+ "/" + packageName
+  githubReleaseUrl := githubRepoUrl + "/releases/download/" + releaseTag
   inv := map[string]string {
     "BinName": packageName,
-    "Version": packageVersion,
-    "PackageUrl64": packageUrl64,
-    "PackageHash64": packageHash64,
-    "PackageUrl32": packageUrl32,
-    "PackageHash32": packageHash32,
     "ClassName": toCamelCase(packageName),
+    "Version": packageVersion,
+    "PackageUrl64": githubReleaseUrl + "/" + binaryPath64,
+    "PackageHash64": hashFileSum(binaryPath64),
+    "PackageUrl32": githubReleaseUrl + "/" + binaryPath32,
+    "PackageHash32": hashFileSum(binaryPath32),
   }
-
-  // Load formula template
   tmpl, _ := Asset("formula.tmpl")
   t := template.New("formula")
   template.Must(t.Parse(string(tmpl)))
   var buf bytes.Buffer
   t.Execute(&buf, inv)
   formula := buf.String()
-  fmt.Println(formula)
 
-  // TODO: Upload to Github
-  ts := oauth2.StaticTokenSource(
-    &oauth2.Token{AccessToken: githubToken},
-  )
+  // Prepare for github API request
+  formulaRepo := "homebrew-" + packageName
+  formulaFile := packageName + ".rb"
+  ts := oauth2.StaticTokenSource(&oauth2.Token{AccessToken: githubToken})
   tc := oauth2.NewClient(oauth2.NoContext, ts)
   client := github.NewClient(tc)
 
-  // release, _, _ := client.Repositories.GetRelease(packageOwner, packageName, 1473977)
-  // fmt.Println(*release.AssetsURL)
+  // Fetch previous file's SHA hash
+  stat, _, _, err := client.Repositories.GetContents(
+    packageOwner,
+    formulaRepo,
+    formulaFile,
+    &github.RepositoryContentGetOptions{}
+  )
+  if err != nil {
+    fmt.Println("Error")
+    os.Exit(1)
+  }
+
+  // Update formula
+  content := &github.RepositoryContentFileOptions{
+    Message: &commitMessage,
+    Content: []byte(formula),
+    SHA: stat.SHA,
+    Committer: &github.CommitAuthor{
+      Name: &commitAuthor,
+      Email: &commitAuthorEmail,
+    },
+  }
+  res, _, err := client.Repositories.UpdateFile(
+    packageOwner,
+    formulaRepo,
+    formulaFile,
+    content
+  )
+  fmt.Println(res)
+  if err != nil {
+    fmt.Println("Error")
+    os.Exit(1)
+  }
 }
--- solver.go
+++ solver.go
@@ -1,17 +1,17 @@
 package main
 
-
 import (
   "os"
   "github.com/codegangsta/cli"
 )
 
+var Commands = []cli.Command{
+  CommandPush,
+}
+
 func main() {
   app := cli.NewApp()
   app.Name = "solver"
-  app.Commands = []cli.Command{
-    CommandPush,
-  }
-
+  app.Commands = Commands
   app.Run(os.Args)
 }
